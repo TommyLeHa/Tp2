493e8d9b6badb13c963357f6bf0298ed3ad9a27a
Tommy Le Ha <leha.tommy.pro@gmail.com>
2020-03-12 19:45:01 -0400

    Ajout des fichiers sass et ajout de la barre menu adaptatif avec 10 choix

--- underscores-enfant/functions.php
+++ underscores-enfant/functions.php
@@ -10,4 +10,16 @@ function wpt_add_google_fonts() {
     }
      
     add_action( 'wp_enqueue_scripts', 'wpt_add_google_fonts' );
+
+/*function extraire_evenement($query){
+if(!is_front_page()&&$query->is_category('evenement'))
+{
+    $query->set('year',2020);
+    $query->set('monthnum_in',1,2,3);
+    $query->set('posts_per_page',-1);
+    $query->set('orderby','date');
+    $query->set('order','asc');
+}
+}
+add_action('pre_get_posts','extraire_evenement');*/
     
